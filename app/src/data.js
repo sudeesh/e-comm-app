53a0beb3d4516f2a4827e377227797789417f88f
sudeesh <sudeesh.b26@gmail.com>
2020-12-31 12:34:33 +0000

    FIrst commit

---
+++ app/src/data.js
@@ -0,0 +1,72 @@
+ const data ={
+    products:[
+        {
+            _id:'1',
+            name:'ponniyinselvan',
+            category: 'books',
+            image: '/images/p1.jpg',
+            price: 100,
+            brand: 'Sud',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Book for old history'
+        },
+        {
+            _id:'2',
+            name:'2',
+            category: 'books',
+            image: '/images/p1.jpg',
+            price: 80,
+            brand: 'Sud',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Book for old history'
+        },
+        {
+            _id:'3',
+            name:'three',
+            category: 'books',
+            image: '/images/p1.jpg',
+            price: 80,
+            brand: 'Sud',
+            rating: 4.5,
+            numReviews: 17,
+            description: 'Book for old history'
+        },
+        {
+            _id:'4',
+            name:'four',
+            category: 'books',
+            image: '/images/p1.jpg',
+            price: 50,
+            brand: 'Sud',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Book for old history'
+        },
+        {
+            _id:'5',
+            name:'five',
+            category: 'books',
+            image: '/images/p1.jpg',
+            price: 100,
+            brand: 'Sud',
+            rating: 4.5,
+            numReviews: 2,
+            description: 'Book for old history'
+        },
+        {
+            _id:'6',
+            name:'six',
+            category: 'books',
+            image: '/images/p1.jpg',
+            price: 100,
+            brand: 'Sud',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Book for old history'
+        },
+    ]
+}
+
+export default data;
