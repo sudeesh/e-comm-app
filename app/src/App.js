0d1473d0c31b10df1858f2542fba215985846a2f
sudeesh <sudeesh.b26@gmail.com>
2021-01-02 04:37:12 +0000

    Added ratimg and product page

--- app/src/App.js
+++ app/src/App.js
@@ -1,3 +1,4 @@
+import Product from "./components/product";
 import data from "./data";
 function App() {
   return (
@@ -15,26 +16,7 @@ function App() {
             <div className="row center">
 
               {data.products.map((product) => 
-                <div key={product._id} className="card">
-                    <a href={`/product/${product._id}`}>
-                        <img className="medium" src={product.image} alt="product" />
-                    </a>
-                    <div className="card-body">
-                        <a href={`/product/${product._id}`}>
-                            <h2>{product.name}</h2>
-                        </a>
-                        <div className="rating">
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                        </div>
-                        <div className="price">
-                            ${product.price}
-                        </div>
-                    </div>
-                </div>
+                <Product key={product._id} product={product} />
                 )}
             </div>
         </main>
